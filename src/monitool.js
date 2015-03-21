1ebf259dbe81c6357339cb2cf48e38ef48a2a153
Adi <kaluza@gmail.com>
2015-03-21 15:49:10 +0100

    Adding Express Framwork. Simple example of usage.

--- src/monitool.js
+++ src/monitool.js
@@ -1,30 +1,61 @@
-/* START************** Includes Block *******************************/
-var http = require('http');
-/* END ************** Includes Block *******************************/
-
-/* START************** Config Block *******************************/
-
-var httpServerPort = 1337;
-var httpServerIPAddress = '127.0.0.1';
-
-
-var mongoIPAddress = "localhost";
-var mongoDBPort = "27017";
-var mongoDBName = "monitooldb";
-
-/* END ************** Config Block *******************************/
 
 
 /**
- * Creation of http server 
+ * @author Adrian Kaluzinski
+ * @date 2015-03-21
+ * How to run monitool-backend:
+ * 
+ * 1. Install Node.js
+ * 2. Install mongoDB
+ * 3. Download necessary packages:
+   npm install mongodb
+   npm install express 
+   4. Run application: 
+     node monitool.js
+
  */
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Default http response\n');
-}).listen(httpServerPort, httpServerIPAddress);
-console.log('HTTP Server running at http://'+httpServerIPAddress+':'+httpServerPort+'/');
 
+var express = require('express'); //Express framework
+var mongodb = require('mongodb'); // mongoDB
+var app = express();
 
-var dbClient = require('./db.js');
-//dbClient.startTestServer(mongoIPAddress, mongoDBPort, mongoDBName);
-//dbClient.startServer(mongoIPAddress, mongoDBPort, mongoDBName);
+var MONGODB_URI = 'mongodb://localhost';
+var applicationPort = 3000;
+var db;
+var coll;
+var dbUtil;
+
+mongodb.MongoClient.connect(MONGODB_URI, {}, function (err, database) {
+    if (err) {
+        throw err;
+    }
+
+    db = database;
+    coll = db.collection('logs');  //set collection from database
+    dbUtil = require('./mongoutils'); //include package with database utils
+    app.listen(applicationPort); //start listening on given port
+    console.log('Listening on port ' + applicationPort);
+});
+
+//simple example of using
+
+app.get('/randoms', function (req, res) {
+    dbUtil.showAllDataFromCollection(coll, req, res);
+});
+
+//app.put('/random', function (req, res) {
+//  dbUtil.insertRandom(coll, req, res);
+//});
+
+//app.delete('/random', function (req, res) {
+//
+//});
+
+app.get('/random', function (req, res) {
+    dbUtil.insertRandom(coll, req, res);
+});
+
+app.get('/cleaner', function (req, res) {
+    dbUtil.cleanDatabase(coll, req, res);
+});
+
